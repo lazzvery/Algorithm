c1c0326c50a64bf32a21a33109825c2acd8d3e69
유미현 <128400949+lazzvery@users.noreply.github.com>
2023-06-25 09:02:49 +0900

    [Silver IV] Title: 동전 0, Time: 180 ms, Memory: 16008 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 11047번 동전 0
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int type = Integer.parseInt(st.nextToken());
+        int price = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[type];
+
+        for(int i = 0; i < type; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+
+        for(int i = type - 1; i >= 0; i--) {
+            while(arr[i] <= price) {
+                price -= arr[i];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
