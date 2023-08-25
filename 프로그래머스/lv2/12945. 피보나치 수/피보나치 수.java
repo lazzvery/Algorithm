e52350d714540d6da9adf95023b2a34dc83dec48
유미현 <128400949+lazzvery@users.noreply.github.com>
2023-08-25 12:22:21 +0900

    [level 2] Title: 피보나치 수, Time: 4.41 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int before1 = 0;
+        int before2 = 1;
+        
+        for(int i = 1; i < n; i++) {
+            answer = (before1 + before2) % 1234567;
+            before1 = before2 % 1234567;
+            before2 = answer % 1234567;
+        }
+        
+        return answer;
+    }
+}
