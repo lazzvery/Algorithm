5a972f6e2728a25540e3afda0a0422e803b602a7
유미현 <128400949+lazzvery@users.noreply.github.com>
2023-08-29 23:14:26 +0900

    [level 2] Title: 구명보트, Time: 10.68 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int index = 0;
+        
+        Arrays.sort(people);
+        
+        for(int i = people.length - 1; i >= index; i--) {
+            if(people[index] + people[i] <= limit) {
+                index++;
+                answer++;
+            } else {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
