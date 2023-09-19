92f39fd1167878eab3b02f4fcc73b4637a8360ef
유미현 <128400949+lazzvery@users.noreply.github.com>
2023-09-19 10:09:17 +0900

    [level 1] Title: 소수 찾기, Time: 14.42 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         boolean[] arr = new boolean[n + 1];
         
-        for(int i = 2; i <= n; i++) {
+        for(int i = 2; i * i <= n; i++) {
             if(!arr[i]) {
                 for(int j = i * 2; j <= n; j+=i) {
                     arr[j] = true;
