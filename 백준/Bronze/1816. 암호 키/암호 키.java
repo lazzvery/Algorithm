4647ed7e2a27dad40b945b8f3005efa60159eea3
유미현 <128400949+lazzvery@users.noreply.github.com>
2023-10-25 11:43:38 +0900

    [Bronze I] Title: 암호 키, Time: 240 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+    문제 1. 암호키 (#1816)
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int cnt = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < cnt; i++) {
+            Long num = Long.parseLong(br.readLine());
+
+            for(int j = 2; j <= 1000001; j++) {
+                if(num % j == 0) {  // 100만 이하의 약수가 존재한다
+                    System.out.println("NO");
+                    break;
+                }
+
+                if(j == 1000000) {  // 100만 이하의 약수가 존재하지 않는다
+                    System.out.println("YES");
+                }
+            }
+        }
+
+    }
+}
